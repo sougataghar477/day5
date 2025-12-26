8bb1c1e7fe89a3465ab90358b38cb41d57ab4ea5
sougata <sougataghar47@gmail.com>
2025-12-26 17:36:24 +0530

    first push

---
+++ js/main.js
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="/css/styles.css"/>
+</head>
+<body>
+    <div class="container">
+
+        <nav class="navigation navigation--desktop">
+            <span class="navigation__logo">Logo</span>
+            <ul class="navigation__links">
+                <li class="navigation__link"><a href="#">Contacts</a></li>
+                <li class="navigation__link"><a href="#">Portfolio</a></li>
+                <li class="navigation__link"><a href="#">Clients</a></li>
+                <li class="navigation__link"><a href="#">About Us</a> </li>
+            </ul>
+        </nav>
+
+        <div class="content">
+        <nav class="navigation navigation--mobile">
+            <div class="top">
+                <div class="content__sidebar__btn">
+                    <label for="checkbox__hidden__for__dashboard">📊</label>
+                    <input id="checkbox__hidden__for__dashboard" type="checkbox"/>
+                </div>
+                <span class="navigation__logo">Logo</span>
+                <div class="hamburger">
+                    <label for="hamburger__checkbox">☰</label>
+                    <input id="hamburger__checkbox" type="checkbox"/>
+                </div>
+
+                </div>
+                <div>
+                <ul class="navigation--mobile__links">
+                    <li class="content__sidebar_link"><a href="#">Contacts</a></li>
+                    <li class="content__sidebar_link"><a href="#">Contacts</a></li>
+                    <li class="content__sidebar_link"><a href="#">Contacts</a></li>
+                    <li class="content__sidebar_link"><a href="#">Contacts</a></li>
+                </ul>
+                </div>
+
+            </div>
+        </nav>
+            <aside class="content__sidebar">
+                <ul class="content__sidebar_links">
+                    <li class="content__sidebar_link"><a href="#">Contacts</a></li>
+                    <li class="content__sidebar_link"><a href="#">Contacts</a></li>
+                    <li class="content__sidebar_link"><a href="#">Contacts</a></li>
+                    <li class="content__sidebar_link"><a href="#">Contacts</a></li>
+                </ul>
+                <label for="checkbox__hidden__for__dashboard" class="close__btn__for_dashboard">✖</label>
+            </aside>
+            <main class="content__main">
+                       <section class="hero">
+            <div class="hero__image">
+                <img src="/assets/pexels-photo-3826666.jpeg"/> 
+            </div>
+            <div class="hero__content">
+                <h1 class="hero__header">
+                    Simple Solutions for Complex Problems
+                </h1>
+                <p>
+                    Showcase your products and connect with your audience. Our all-in-one platform helps you manage operations and boost visibility — whether you're in fashion, beauty, wellness, or beyond.
+                </p>
+                <button class="hero__cta">Get Started</button>
+
+            </div>
+        </section>
+        <section class="cards">
+            <div class="card">
+                <div class="card__image">
+                    <img src="/assets/pexels-photo-3826666.jpeg"/>
+                </div>
+                <div class="card__content">
+                    <h1 card__content__header>Card Header</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card__image">
+                    <img src="/assets/pexels-photo-3826666.jpeg"/>
+                </div>
+                <div class="card__content">
+                    <h1 card__content__header>Card Header</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card__image">
+                    <img src="/assets/pexels-photo-3826666.jpeg"/>
+                </div>
+                <div class="card__content">
+                    <h1 card__content__header>Card Header</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                </div>
+            </div>
+                        <div class="card">
+                <div class="card__image">
+                    <img src="/assets/pexels-photo-3826666.jpeg"/>
+                </div>
+                <div class="card__content">
+                    <h1 card__content__header>Card Header</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                </div>
+            </div>
+                        <div class="card">
+                <div class="card__image">
+                    <img src="/assets/pexels-photo-3826666.jpeg"/>
+                </div>
+                <div class="card__content">
+                    <h1 card__content__header>Card Header</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                </div>
+            </div>
+                        <div class="card">
+                <div class="card__image">
+                    <img src="/assets/pexels-photo-3826666.jpeg"/>
+                </div>
+                <div class="card__content">
+                    <h1 card__content__header>Card Header</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                </div>
+            </div>
+        </section> 
+            </main>
+        </div>
+        
+    </div>
+    <script src="/js/main.js"></script>
+</body>
+</html>
